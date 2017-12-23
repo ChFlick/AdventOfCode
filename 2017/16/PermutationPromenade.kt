a2a63a0b941d655a72681e38c8ab6bdfe9c3d6e4
ChFlick <christophflick@gmx.de>
2017-12-23 10:26:07 +0100

    Improved solution 16

--- 2017/16/PermutationPromenade.kt
+++ 2017/16/PermutationPromenade.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
     val times = 1000000000
     var programs = "abcdefghijklmnop"
-    val actions = readLine()?.split(",")
+    val actions = readLine()?.split(",") ?: ArrayList()
     var occ1 = 0
     var occ2 = 0
 
@@ -27,47 +27,32 @@ fun main(args: Array<String>) {
     println(programs)
 }
 
-private fun dance(actions: List<String>?, programs: String): String {
-    var programs1 = programs
-    actions?.forEach {
-        programs1 = when {
-            it.startsWith("s") ->
-                spin(programs1, Integer.valueOf(it.substring(1)))
-            it.startsWith("x") -> {
-                val positions = it.substring(1).split("/")
-                exchange(programs1, Integer.valueOf(positions[0]), Integer.valueOf(positions[1]))
+private fun dance(actions: List<String>, programs: String): String =
+        actions.fold(programs) { programs, it ->
+            when {
+                it.startsWith("s") ->
+                    spin(programs, Integer.valueOf(it.substring(1)))
+                it.startsWith("x") -> {
+                    val positions = it.substring(1).split("/")
+                    exchange(programs, Integer.valueOf(positions[0]), Integer.valueOf(positions[1]))
+                }
+                it.startsWith("p") -> {
+                    val partners = it.substring(1).split("/")
+                    partner(programs, partners[0], partners[1])
+                }
+                else -> "ERROR"
             }
-            it.startsWith("p") -> {
-                val partners = it.substring(1).split("/")
-                partner(programs1, partners[0], partners[1])
-            }
-            else -> "ERROR"
         }
-    }
-    return programs1
-}
 
-fun spin(string: String, times: Int): String {
-    var result: String = string
-    for(i in 0 until times){
-        result = result.takeLast(1) + result.substring(0, result.length - 1)
-    }
+fun spin(string: String, times: Int): String =
+    (0 until times).fold(string) { acc, _ -> acc.takeLast(1) + acc.substring(0, acc.length - 1) }
 
-    return result
-}
-
-fun exchange(string: String, posA: Int, posB: Int): String {
-    if(posA == posB) return string
-
-    val lowerPos = minOf(posA, posB)
-    val higherPos = maxOf(posA, posB)
-
-    return string.substring(0, lowerPos) +
-            string[higherPos] +
-            string.substring(lowerPos + 1, higherPos) +
-            string[lowerPos] +
-            string.substring(higherPos + 1, string.length)
-}
+fun exchange(string: String, posA: Int, posB: Int): String =
+    string.substring(0, minOf(posA, posB)) +
+            string[maxOf(posA, posB)] +
+            string.substring(minOf(posA, posB) + 1, maxOf(posA, posB)) +
+            string[minOf(posA, posB)] +
+            string.substring(maxOf(posA, posB) + 1, string.length)
 
 fun partner(string: String, partnerA: String, partnerB: String): String =
      exchange(string, string.indexOf(partnerA), string.indexOf(partnerB))
