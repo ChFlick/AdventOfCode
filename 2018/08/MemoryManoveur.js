717f3a9ee66b48e1011fad21f3d6ee3a9c546324
Flickster <christophflick@gmx.de>
2018-12-18 08:23:46 +0100

    Add solution for 2018-08.1

---
+++ 2018/08/MemoryManoveur.js
@@ -0,0 +1,70 @@
+var fs = require("fs"),
+    path = require("path"),
+    filePath = path.join(__dirname, 'input.txt');
+
+fs.readFile(filePath, {
+    encoding: 'utf-8'
+}, function (err, data) {
+    if (!err) {
+        values = data.split(" ").map(v => parseInt(v));
+
+        const {currentNode: tree} = createTree(values, 0);
+        
+        const metadataValue = getTreeMetadataSum(tree);
+        console.log(metadataValue);
+    }
+});
+
+class Node {
+    constructor() {
+        this.childs = [];
+        this.metadata = [];
+    }
+
+    addChild(node) {
+        this.childs.push(node);
+    }
+
+    addMetadata(value) {
+        this.metadata.push(value);
+    }
+
+    getChilds() {
+        return this.childs;
+    }
+
+    getMetadata() {
+        return this.metadata;
+    }
+}
+
+function createTree(values, position) {
+    const childNodeCount = values[position];
+    position++;
+
+    const metadataCount = values[position];
+    position++;
+
+    const currentNode = new Node();
+    for(let i = 0; i < childNodeCount; i++) {
+        const { currentNode: childNode, position: newPosition } = createTree(values, position);
+        currentNode.addChild(childNode);
+        position = newPosition;
+    }
+
+    for (let i = 0; i < metadataCount; i++) {
+        currentNode.addMetadata(values[position]);
+        position++;
+    }
+    
+    return {currentNode, position};
+}
+
+function getTreeMetadataSum(tree) {
+    let sum = 0;
+
+    sum += tree.getMetadata().reduce((s, curr) => s += curr, 0);
+    sum += tree.getChilds().reduce((s, curr) => s += getTreeMetadataSum(curr), 0);
+
+    return sum;
+}
