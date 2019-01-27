95b9e5185f2ab5d30a17f9f4bc2854a3dc84087f
Christoph Flick <christophflick@gmx.de>
2019-01-27 22:54:18 +0100

    Add map loading and minecart generation

--- 2018/13/MinecartMadness.ts
+++ 2018/13/MinecartMadness.ts
@@ -1,3 +1,6 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
 enum Direction {
     Up,
     Right,
@@ -10,6 +13,39 @@ interface Point{
     y: number;
 }
 
+class Map {
+    private map: string[];
+
+    constructor(fileName: string) {
+        let fileBuffer: string = fs.readFileSync(path.resolve(__dirname, 'testInput.txt'));
+        this.map = fileBuffer.toString().split('\r\n');
+        console.log("Read map:\n", this.map);
+    }
+
+    getMinecarts(): Minecart[] {
+        let minecarts: Minecart[] = [];
+
+        this.map.forEach((line, y) => {
+            line.split('').forEach((mapElement, x) => {
+                if(mapElement === '^') {
+                    minecarts.push(new Minecart(Direction.Up, {x, y}));
+                }
+                else if(mapElement === '>') {
+                    minecarts.push(new Minecart(Direction.Right, {x, y}));
+                }
+                else if(mapElement === 'v') {
+                    minecarts.push(new Minecart(Direction.Down, {x, y}));
+                }
+                else if(mapElement === '<') {
+                    minecarts.push(new Minecart(Direction.Left, {x, y}));
+                }
+            });
+        });
+
+        return minecarts;
+    }
+}
+
 class Minecart {
     private direction: Direction;
     private position: Point;
@@ -18,11 +54,29 @@ class Minecart {
         this.direction = direction;
         this.position = position;
     }
+
+    move(): void {
+        switch (this.direction) {
+            case Direction.Up:
+                this.position.y -= 1;
+                break;
+            case Direction.Down:
+                this.position.y += 1;
+                break;
+            case Direction.Left:
+                this.position.x -= 1;
+                break;
+            case Direction.Right:
+                this.position.x += 1;
+                break;
+        }
+    }
 }
 
+const INPUT_PATH = "testInput.txt";
 
-let dir = Direction.Down;
-let pos = {x: 10, y: 10};
-let minecart = new Minecart(dir, pos);
 
-console.log(minecart);
+let map = new Map(INPUT_PATH);
+const minecarts = map.getMinecarts();
+
+console.log(minecarts);
