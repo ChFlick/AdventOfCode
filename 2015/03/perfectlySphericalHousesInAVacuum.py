99ef668145a34cc5ebcc1cbe81712b741e512e98
Christoph Flick <christophflick@gmx.de>
2019-06-15 22:38:31 +0200

    Add solution for 2015-03.2

--- 2015/03/perfectlySphericalHousesInAVacuum.py
+++ 2015/03/perfectlySphericalHousesInAVacuum.py
@@ -1,6 +1,13 @@
 filename: str = "input.txt"
 content: str
 
+directions = {
+    '<': lambda pos: (pos[0] - 1, pos[1]),
+    '>': lambda pos: (pos[0] + 1, pos[1]),
+    'v': lambda pos: (pos[0], pos[1] - 1),
+    '^': lambda pos: (pos[0], pos[1] + 1)
+}
+
 with open(filename, "r") as inputData:
     content = inputData.readline()
 
@@ -9,12 +16,28 @@ position = (0, 0)
 
 visited.add(position)
 for direction in content:
-    position = {
-        '<': lambda pos: (pos[0] - 1, pos[1]),
-        '>': lambda pos: (pos[0] + 1, pos[1]),
-        'v': lambda pos: (pos[0], pos[1] - 1),
-        '^': lambda pos: (pos[0], pos[1] + 1)
-    }[direction](position)
+    position = directions[direction](position)
     visited.add(position)
+
+print(len(visited))
+
+#Part 2
+visited: set = set()
+secVisited: set = set()
+
+position = (0, 0)
+secPosition = (0, 0)
+
+visited.add(position)
+
+i = 0
+for direction in content:
+    if i % 2 == 0:
+        position = directions[direction](position)
+        visited.add(position)
+    else:
+        secPosition = directions[direction](secPosition)
+        visited.add(secPosition)
+    i = i + 1
 
 print(len(visited))
