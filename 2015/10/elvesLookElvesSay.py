8eb5f92e58165ebfc15541eb8ddb2fdf3f2a3185
Christoph Flick <christophflick@gmx.de>
2019-06-17 12:10:12 +0200

    Add solution for 2015-10.1

---
+++ 2015/10/elvesLookElvesSay.py
@@ -0,0 +1,27 @@
+def lookAndSay(input):
+    result = ""
+
+    i = 0
+    while i < len(input):
+        currCount = 0
+        curr = input[i]
+        for j in range(i, len(input)):
+            if input[j] == curr:
+                currCount = currCount + 1
+                if j == len(input) - 1:
+                    i = j
+            else:
+                i = j - 1
+                break
+        
+        result = result + str(currCount) + str(curr)
+        i = i + 1
+
+    return result
+
+currentValue = "1321131112"
+for _ in range(40):
+    currentValue = lookAndSay(currentValue)
+    #print(currentValue)
+#print(currentValue)
+print(len(currentValue))
