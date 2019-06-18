262d71191c8da453baa59f532c2188a238e7800e
Flickster <christophflick@gmx.de>
2019-06-18 08:10:33 +0200

    Add solution for 2015-12.2

--- 2015/12/jsAbacusFrameworkIO2.py
+++ 2015/12/jsAbacusFrameworkIO2.py
@@ -1,9 +1,23 @@
 import re
 import json
+from functools import reduce
 
 filename = "input.txt"
+
+def addNumberNotRed(node):
+    if type(node) is dict:
+        return 0 if reduce(lambda x,y: x or y, [node.get(x) == "red" for x in node]) else reduce(lambda x,y: x + y, [addNumberNotRed(node.get(x)) for x in node])
+    if type(node) is list:
+        return reduce(lambda x,y: x + y, [addNumberNotRed(x) for x in node])
+    if type(node) is str:
+        return 0
+    if type(node) is int:
+        return node
+
 
 with open(filename, "r") as inputData:
     content: str = inputData.readline()
 
-print([x for x in json.loads(content)[0]])
+print(addNumberNotRed(json.loads(content)))
+# print(addNumberNotRed([1, 2, [3, "red", 4]]))
+# print(addNumberNotRed([1,{"c":"red","b":2},3]))
