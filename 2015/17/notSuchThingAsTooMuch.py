5469e61b3f58efaa845c32eb9ef8f96299b9c7a2
Christoph Flick <christophflick@gmx.de>
2019-06-20 11:07:23 +0200

    Add solution for 2015-17.1

--- 2015/17/notSuchThingAsTooMuch.py
+++ 2015/17/notSuchThingAsTooMuch.py
@@ -1,18 +1,28 @@
-from itertools import permutations
-
-containers = [33, 14, 18, 20, 45, 35, 16, 35, 1, 13, 18, 13, 50, 44, 48, 6, 24, 41, 30, 42]
+from itertools import groupby
 
 TARGET = 150
+# TARGET = 25
 possibilities = []
 
-for perm in permutations(containers):
-    sum = 0
-    for i in range(len(perm)):
-        sum = sum + perm[i]
-        if sum == TARGET:
-            possibilities.append(perm[0:i])
-        elif sum > TARGET:
-            break
+def sumsUp(numbers, partial):
+    val = sum(partial)
+    if val == TARGET:
+        possibilities.append(partial)
+        return
+    elif val > TARGET:
+        return
+
+    for i in range(len(numbers)):
+        n = numbers[i]
+        rest = numbers[i+1:]
+        sumsUp(rest, partial + [n])
+
+containers = [33, 14, 18, 20, 45, 35, 16, 35, 1, 13, 18, 13, 50, 44, 48, 6, 24, 41, 30, 42]
+# containers = [20, 15, 10, 5, 5]
+
+sumsUp(containers, [])
 
 print(possibilities)
 print(len(possibilities))
+
+
