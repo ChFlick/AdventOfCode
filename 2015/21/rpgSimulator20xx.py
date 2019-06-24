c4a804f886291c921252c1b5db0e59034a2d15a2
Christoph Flick <flick@bitside.de>
2019-06-24 15:10:19 +0200

    Add solution for 2015-21.1

--- 2015/21/rpgSimulator20xx.py
+++ 2015/21/rpgSimulator20xx.py
@@ -1,3 +1,5 @@
+from itertools import permutations
+
 BOSSHP = 103
 BOSSDMG = 9
 BOSSARM = 2
@@ -9,12 +11,13 @@ weapons = [
     (40, 7, 0),
     (74, 8, 0)
 ]
-armor = [
+armors = [
     (13, 0, 1),
     (31, 0, 2),
     (53, 0, 3),
     (75, 0, 4),
-    (102, 0, 5)
+    (102, 0, 5),
+    (0, 0, 0)
 ]
 rings = [
     (25, 1, 0),
@@ -22,7 +25,9 @@ rings = [
     (100, 3, 0),
     (20, 0, 1),
     (40, 0, 2),
-    (80, 0, 3)
+    (80, 0, 3),
+    (0, 0, 0),
+    (0, 0, 0)
 ]
 
 
@@ -36,13 +41,25 @@ def fight(player, boss):
     while player.hp > 0 or boss.hp > 0:
         boss.hp = boss.hp - (player.damage - boss.armor)
         player.hp = player.hp - (boss.damage - player.armor)
-        print("boss", boss.hp, "player", player.hp)
+        # print("boss", boss.hp, "player", player.hp)
         if boss.hp < 1:
             return "player"
         elif player.hp < 1:
             return "boss"
 
-player = GameEntity(100, 11, 0)
-boss = GameEntity(BOSSHP, BOSSDMG, BOSSARM)
+minCosts = 9999999
+for weapon in weapons:
+    for armor in armors:
+        for ringperms in permutations(rings):
+            costs = weapon[0] + armor[0] + ringperms[0][0] + ringperms[1][0]
+            if costs > minCosts:
+                continue
 
-print(fight(player, boss))
+            damage = weapon[1] + armor[1] + ringperms[0][1] + ringperms[1][1]
+            arm = weapon[2] + armor[2] + ringperms[0][2] + ringperms[1][2]
+            player = GameEntity(100, damage, arm)
+            boss = GameEntity(BOSSHP, BOSSDMG, BOSSARM)
+            if fight(player, boss) == "player":
+                minCosts = min([minCosts, costs])
+
+print(minCosts)
