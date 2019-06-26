82dc2f5b69105f79eec5413c8c845960d4a20194
Christoph Flick <christophflick@gmx.de>
2019-06-26 08:25:47 +0200

    First solution take far too long

--- 2015/24/itHangsInTheBalance.py
+++ 2015/24/itHangsInTheBalance.py
@@ -1,4 +1,5 @@
 from itertools import permutations
+from functools import reduce
 
 weights = [1, 2, 3, 7, 11, 13, 17, 19, 23, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113]
 # weights = [1, 2, 3, 4, 5, 7, 8, 9, 10, 11]
@@ -6,6 +7,10 @@ weights = [1, 2, 3, 7, 11, 13, 17, 19, 23, 31, 37, 41, 43, 47, 53, 59, 61, 67, 7
 groupWeight = sum(weights) / 3
 
 possibilities = set()
+seenRest = set()
+
+def quantumEntanglement(tup):
+    return reduce(lambda x,y: x*y, tup)
 
 def findGroup(currentTree, rest, maxSum):
     if len(rest) == 0:
@@ -14,17 +19,20 @@ def findGroup(currentTree, rest, maxSum):
         return
     if sum(currentTree) == maxSum:
         currentTree.sort()
-        possibilities.add(tuple(currentTree))
-        #  skip if already added
-        findGroup([], rest, maxSum)
+        if tuple(currentTree) not in possibilities:
+            possibilities.add(tuple(currentTree))
+            if tuple(rest) not in seenRest:
+                seenRest.add(tuple(rest))
+                findGroup([], rest, maxSum)
     
     for i in range(len(rest)):
         findGroup(currentTree + [rest[i]], rest[:i] + rest[i + 1:], maxSum)
 
 findGroup([], weights, groupWeight)
-# print(len(possibilities))
-# print(possibilities)
 
 minSize = len(min(possibilities, key=len))
 minPossibilities = [x for x in possibilities if len(x) == minSize]
 print(minPossibilities)
+print(len(minPossibilities))
+print([quantumEntanglement(x) for x in minPossibilities])
+print(min([quantumEntanglement(x) for x in minPossibilities]))
