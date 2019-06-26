270d39bc3b961f6c1a5969a760156d1ec9daa0ec
Christoph Flick <christophflick@gmx.de>
2019-06-26 08:56:16 +0200

    Add solution for 2015-24.1

--- 2015/24/itHangsInTheBalance.py
+++ 2015/24/itHangsInTheBalance.py
@@ -1,6 +1,9 @@
-from itertools import permutations
+from itertools import combinations
 from functools import reduce
 import time
+
+def quantumEntanglement(iterable):
+    return reduce(lambda x,y: x*y, iterable)
 
 weights = [1, 2, 3, 7, 11, 13, 17, 19, 23, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113]
 # weights = [1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13 ,14]
@@ -8,42 +11,13 @@ weights = [1, 2, 3, 7, 11, 13, 17, 19, 23, 31, 37, 41, 43, 47, 53, 59, 61, 67, 7
 weights.reverse()
 
 groupWeight = sum(weights) / 3
+combies = []
 
-possibilities = set()
-seenRest = set()
-minLen = 999
+for i in range(1, 8):
+    if len(combies) > 0:
+        break
+    for comb in combinations(weights, i):
+        if sum(comb) == groupWeight:
+            combies.append(comb)
 
-def quantumEntanglement(tup):
-    return reduce(lambda x,y: x*y, tup)
-
-def findGroup(currentTree, rest, maxSum):
-    global minLen
-
-    if len(currentTree) > minLen:
-        return
-    if len(rest) == 0:
-        return
-    if sum(currentTree) > maxSum:
-        return
-    if sum(currentTree) == maxSum:
-        currentTree.sort()
-        if tuple(currentTree) not in possibilities:
-            possibilities.add(tuple(currentTree))
-            minLen = min([minLen, len(currentTree)])
-            if tuple(rest) not in seenRest:
-                seenRest.add(tuple(rest))
-                findGroup([], rest, maxSum)
-    
-    for i in range(len(rest)):
-        findGroup(currentTree + [rest[i]], rest[i + 1:], maxSum)
-
-findGroup([], weights, groupWeight)
-
-print(possibilities)
-
-minSize = len(min(possibilities, key=len))
-minPossibilities = [x for x in possibilities if len(x) == minSize]
-# print(minPossibilities)
-print(len(minPossibilities))
-# print([quantumEntanglement(x) for x in minPossibilities])
-print(min([quantumEntanglement(x) for x in minPossibilities]))
+print(min([quantumEntanglement(x) for x in combies]))
