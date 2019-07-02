234b41ce0bfcc3e5a47d27cb30ab682a49b92e85
Christoph Flick <flick@bitside.de>
2019-07-02 13:45:14 +0200

    Add solution for 2016-06.1

---
+++ 2016/06/signalsAndNoise.ts
@@ -0,0 +1,21 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+const fileBuffer: string = fs.readFileSync(path.resolve(__dirname, 'input.txt'));
+const rows: string[] = fileBuffer.toString().split('\n');
+const cols: string[] = [];
+for(let i = 0; i < rows[0].length; i++) {
+    let curr = rows.reduce((sum, curr) => sum += curr.charAt(i), "");
+    cols.push(curr)
+}
+
+const charNumbersPerCol = cols.map(col => col.split("")
+    .reduce((charNums, char) => {
+        charNums[char] = charNums[char] ? charNums[char] + 1 : 1;
+        return charNums;
+    }, {})
+)
+
+const maxNums = charNumbersPerCol.map(charNums => Object.keys(charNums).reduce((max, curr) => charNums[curr] > max[1] ? [curr, charNums[curr]] : max, ["", 0]))
+
+console.log(maxNums.reduce((s, c) => s + c[0], ""));
