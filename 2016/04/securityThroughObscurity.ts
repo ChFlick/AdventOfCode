43430c70b30d64dcdee30e6c28b63eff51fe8383
Christoph Flick <flick@bitside.de>
2019-07-02 10:31:14 +0200

    Add solution for 2016-04.1

--- 2016/04/securityThroughObscurity.ts
+++ 2016/04/securityThroughObscurity.ts
@@ -4,18 +4,19 @@ import * as path from 'path';
 type CharSum = [string, number];
 
 class Room {
-    readonly room: string;
-    readonly statedChecksum: string;
-    readonly realChecksum: string;
+    private readonly room: string;
+    private readonly statedChecksum: string;
+    private readonly realChecksum: string;
+    private readonly sectorId: number;
 
     constructor(room: string, statedChecksum: string) {
         this.room = room;
+        this.sectorId = parseInt(room.match(/\d+/)[0]);
         this.statedChecksum = statedChecksum;
         this.realChecksum = this.calculateChecksum(room);
     }
 
-    calculateChecksum = (room: string): string => {
-        let checksum = "";
+    private calculateChecksum = (room: string): string => {
         const sums: CharSum[] = room.split("")
             .filter((char: string) => char.match(/[a-z]/))
             .reduce((sums: CharSum[], char: string) => {
@@ -35,9 +36,16 @@ class Room {
     isReal(): boolean {
         return this.statedChecksum === this.realChecksum;
     }
+
+    getSectorId(): number {
+        return this.sectorId;
+    }
 }
 
 const fileBuffer: string = fs.readFileSync(path.resolve(__dirname, 'input.txt'));
 const rooms: Room[] = fileBuffer.toString().split('\n')
     .map(line => line.split("["))
     .map(split => new Room(split[0], split[1].substr(0, split[1].length - 1)));
+
+const validSum = rooms.reduce((sum, room) => sum += room.isReal() ? room.getSectorId() : 0, 0);
+console.log(validSum);
