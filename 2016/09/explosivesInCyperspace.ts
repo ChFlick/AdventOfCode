b1673450397919d0791ff0da5f9537514bf7646f
Christoph Flick <flick@bitside.de>
2019-07-03 11:40:45 +0200

    Add solution for 2016-09.1

---
+++ 2016/09/explosivesInCyperspace.ts
@@ -0,0 +1,26 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+const fileBuffer: string = fs.readFileSync(path.resolve(__dirname, 'input.txt'));
+const compressed: string = fileBuffer.toString();
+
+let decompressed = "";
+let pos = 0;
+
+
+while (pos < compressed.length) {
+    if (compressed[pos].match(/[A-Z]/)) {
+        decompressed += compressed[pos];
+        pos++;
+        continue;
+    }
+    
+    const marker = compressed.substr(pos).match(/\(.*?\)/)[0];
+    const [chars, times] = marker.replace("(", "").replace(")", "").split("x");
+    decompressed += compressed.substr(pos + marker.length, parseInt(chars)).repeat(parseInt(times));
+    pos += marker.length + parseInt(chars);
+}
+
+console.log(decompressed);
+console.log(decompressed.length);
+
