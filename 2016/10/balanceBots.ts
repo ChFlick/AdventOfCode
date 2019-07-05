b688aea843181974426e66b516533f58de86937e
Christoph Flick <flick@bitside.de>
2019-07-05 11:04:55 +0200

    Add solution for 2016-10.1

--- 2016/10/balanceBots.ts
+++ 2016/10/balanceBots.ts
@@ -35,23 +35,70 @@ class Bot {
 }
 
 const fileBuffer: Buffer = fs.readFileSync(path.resolve(__dirname, 'input.txt'));
-const instructions: string[] = fileBuffer.toString().split("\n");
+let instructions: string[] = fileBuffer.toString().split("\n");
 
 const bots: Bot[] = []
+const outputs: number[] = "0".repeat(30).split("").map(x => parseInt(x))
 
-for (const instruction of instructions) {
+for (const instruction of instructions.filter(i => i.startsWith("value"))) {
     const parts = instruction.split(" ");
-    if (instruction.startsWith("value")) {
-        const botId = parseInt(parts[5]);
-        const bot: Bot = bots.find(bot => bot.botId === botId);
-        if(!bot) {
-            bots.push(new Bot(botId, parseInt(parts[1])))
-        } else {
-            bot.valueTwo = parseInt(parts[1]);
-        }
+    const botId = parseInt(parts[5]);
+    const bot: Bot = bots.find(bot => bot.botId === botId);
+    if(!bot) {
+        bots.push(new Bot(botId, parseInt(parts[1])))
+    } else {
+        bot.valueTwo = parseInt(parts[1]);
     }
 }
 
-console.log(bots);
-console.log(bots.map(bot => bot.lowVal()));
+//value connections
+instructions = instructions.filter(i => i.startsWith("bot"));
+while (instructions.length > 0) {
+    let restInstructions = [];
+    for (const instruction of instructions) {
+        
+        const parts = instruction.split(" ");
+        const givingBotId = parseInt(parts[1]);
+        const givingBot: Bot = bots.find(bot => bot.botId === givingBotId);
+        if(!givingBot) {
+            restInstructions.push(instruction);
+            continue;
+        }
 
+        if(!(givingBot.valueOne && givingBot.valueTwo)){
+            restInstructions.push(instruction);
+            continue;
+        }
+
+        const receivingLowId = parseInt(parts[6])
+        if(parts[5] === "output") {
+            outputs[receivingLowId] = givingBot.lowVal();
+        } else {
+            const receivingLowBot: Bot = bots.find(bot => bot.botId == receivingLowId);
+            if(!receivingLowBot) {
+                bots.push(new Bot(receivingLowId, givingBot.lowVal()))
+            } else {
+                receivingLowBot.valueTwo = givingBot.lowVal();
+            }
+        }
+
+        const receivingHighId = parseInt(parts[11]);
+        if(parts[10] === "output") {
+            outputs[receivingHighId] = givingBot.highVal();
+        } else {
+            const receivingHighBot: Bot = bots.find(bot => bot.botId == receivingHighId);
+            if(!receivingHighBot) {
+                bots.push(new Bot(receivingHighId, givingBot.highVal()))
+            } else {
+                receivingHighBot.valueTwo = givingBot.highVal();
+            }
+        }
+    }
+    instructions = restInstructions;
+}
+
+// console.log(bots);
+// console.log(outputs);
+
+console.log(bots.find(bot => bot.valueOne === 17 && bot.valueTwo === 61))
+console.log(bots.find(bot => bot.valueOne === 61 && bot.valueTwo === 17))
