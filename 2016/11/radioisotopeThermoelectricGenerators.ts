48f1864f1c1b3af07356fc221e2771a8881c90da
Christoph Flick <christophflick@gmx.de>
2019-07-16 00:08:29 +0200

    Working towards 2016-11.1

--- 2016/11/radioisotopeThermoelectricGenerators.ts
+++ 2016/11/radioisotopeThermoelectricGenerators.ts
@@ -11,15 +11,67 @@ function isValid(floors: string[][]) {
     return true;
 }
 
-const seenSetups = new Set<string[][]>();
+function floorsHash(floors: string[][]) {
+    return floors.map(floor => floor.sort().join(" ")).join("-")
+}
+
+const seenSetups = new Map<string, number>();
+
+let minSteps = 500;
+
 const floors = [["THG", "THM", "PLG", "STG"],
 ["PLM", "STM"],
 ["PRG", "PRM", "RUG", "RUM"],
 []];
 
-seenSetups.add(floors);
-console.log(goalReached(floors));
-console.log(isValid(floors));
+function findValidGoal(floors: string[][], elevatorFloor: number = 0, steps: number = 0) {
+    if (steps > minSteps) { return; }
 
+    if (!isValid(floors)) { return; }
 
+    if (goalReached(floors)) {
+        console.log("Goal reached");
+        minSteps = Math.min(steps, minSteps);
+        console.log(minSteps);
+        return;
+    }
 
+    if (!seenSetups.has(floorsHash(floors)) || seenSetups.get(floorsHash(floors)) > steps) {
+        seenSetups.set(floorsHash(floors), steps);
+    } else {
+        return;
+    }
+
+    const currentFloor = floors[elevatorFloor];
+    const possibilities: string[][] = [];
+    for (let i = 0; i < currentFloor.length; i++) {
+        const first = currentFloor[i];
+        possibilities.push([first]);
+        for (let j = i + 1; j < currentFloor.length; j++) {
+            possibilities.push([first, currentFloor[j]]);
+        }
+    }
+
+    for (const possibility of possibilities) {
+        const oldFloor = currentFloor.filter(ele => !possibility.includes(ele));
+
+        if (elevatorFloor === 0) {
+            setTimeout(findValidGoal, 0, [oldFloor, [...floors[1], ...possibility], floors[2], floors[3]], 1, steps + 1);
+        }
+        else if (elevatorFloor === 1) {
+            setTimeout(findValidGoal, 0, [[...floors[0], ...possibility], oldFloor, floors[2], floors[3]], 0, steps + 1);
+            setTimeout(findValidGoal, 0, [floors[0], oldFloor, [...floors[2], ...possibility], floors[3]], 2, steps + 1);
+        }
+        else if (elevatorFloor === 2) {
+            setTimeout(findValidGoal, 0, [floors[0], [...floors[1], ...possibility], oldFloor, floors[3]], 1, steps + 1);
+            setTimeout(findValidGoal, 0, [floors[0], floors[1], oldFloor, [...floors[3], ...possibility]], 3, steps + 1);
+        }
+        else if (elevatorFloor === 3) {
+            setTimeout(findValidGoal, 0, [floors[0], floors[1], [...floors[2], ...possibility], oldFloor], 2, steps + 1);
+        }
+
+    }
+}
+
+findValidGoal(floors);
+console.log(minSteps);
