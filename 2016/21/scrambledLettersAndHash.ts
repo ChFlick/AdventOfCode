9e6cabdfde7e5e4569d597299e19d6ac59d139aa
Christoph Flick <flick@bitside.de>
2019-07-24 14:39:44 +0200

    Add solution for 2016-21.1

---
+++ 2016/21/scrambledLettersAndHash.ts
@@ -0,0 +1,58 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+const swapAt = (str: string, x: number, y: number) => {
+    const temp = str.split("");
+    const xChar = temp[x];
+    temp[x] = temp[y];
+    temp[y] = xChar;
+
+    return temp.join("");
+}
+
+const swapLetters = (str: string, x: string, y: string) => swapAt(str, str.indexOf(x), str.indexOf(y));
+
+const reverse = (str: string, from: number, to: number) =>
+    str.substr(0, from) + str.substring(from, to + 1).split("").reverse().join("") + str.substr(to + 1);
+
+const rotateLeft = (str: string, amount: number) => str.substr(amount % str.length) + str.substr(0, amount % str.length);
+const rotateRight = (str: string, amount: number) => rotateLeft(str, str.length - (amount % str.length));
+
+const move = (str: string, from: number, to: number) => {
+    const strWithout = str.replace(str[from], "");
+    return strWithout.substr(0, to) + str[from] + strWithout.substr(to)
+};
+
+const rotateBased = (str: string, char: string) => rotateRight(str, 1 + (str.indexOf(char) < 4 ? str.indexOf(char) : str.indexOf(char) + 1));
+
+let letters = 'abcdefgh';
+
+const fileBuffer: Buffer = fs.readFileSync(path.resolve(__dirname, 'input.txt'));
+const instructions: string[] = fileBuffer.toString().split("\n");
+
+for (const instruction of instructions) {
+    const parts = instruction.split(" ");
+    if (instruction.startsWith('swap position')) {
+        letters = swapAt(letters, parseInt(parts[2]), parseInt(parts[5]));
+    }
+    else if (instruction.startsWith('swap letter')) {
+        letters = swapLetters(letters, parts[2], parts[5]);
+    }
+    else if (instruction.startsWith('reverse')) {
+        letters = reverse(letters, parseInt(parts[2]), parseInt(parts[4]));
+    }
+    else if (instruction.startsWith('rotate left')) {
+        letters = rotateLeft(letters, parseInt(parts[2]));
+    }
+    else if (instruction.startsWith('rotate right')) {
+        letters = rotateRight(letters, parseInt(parts[2]));
+    }
+    else if (instruction.startsWith('move')) {
+        letters = move(letters, parseInt(parts[2]), parseInt(parts[5]));
+    }
+    else if (instruction.startsWith('rotate based')) {
+        letters = rotateBased(letters, parts[6]);
+    }
+}
+
+console.log(letters);
