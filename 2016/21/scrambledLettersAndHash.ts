7aabbb22cfa8b1a0cf6671686855f059f89d9bdd
Christoph Flick <flick@bitside.de>
2019-07-24 14:53:35 +0200

    Start 2016-21.2

--- 2016/21/scrambledLettersAndHash.ts
+++ 2016/21/scrambledLettersAndHash.ts
@@ -23,7 +23,8 @@ const move = (str: string, from: number, to: number) => {
     return strWithout.substr(0, to) + str[from] + strWithout.substr(to)
 };
 
-const rotateBased = (str: string, char: string) => rotateRight(str, 1 + (str.indexOf(char) < 4 ? str.indexOf(char) : str.indexOf(char) + 1));
+const rotateBasedRight = (str: string, char: string) => rotateRight(str, 1 + (str.indexOf(char) < 4 ? str.indexOf(char) : str.indexOf(char) + 1));
+const rotateBasedLeft = (str: string, char: string) => rotateLeft(str, 1 + (str.indexOf(char) > 3 ? str.indexOf(char) : str.indexOf(char) + 1));
 
 let letters = 'abcdefgh';
 
@@ -51,7 +52,35 @@ for (const instruction of instructions) {
         letters = move(letters, parseInt(parts[2]), parseInt(parts[5]));
     }
     else if (instruction.startsWith('rotate based')) {
-        letters = rotateBased(letters, parts[6]);
+        letters = rotateBasedRight(letters, parts[6]);
+    }
+}
+
+console.log(letters);
+
+// reverse
+for (const instruction of instructions.reverse()) {
+    const parts = instruction.split(" ");
+    if (instruction.startsWith('swap position')) {
+        letters = swapAt(letters, parseInt(parts[2]), parseInt(parts[5]));
+    }
+    else if (instruction.startsWith('swap letter')) {
+        letters = swapLetters(letters, parts[2], parts[5]);
+    }
+    else if (instruction.startsWith('reverse')) {
+        letters = reverse(letters, parseInt(parts[2]), parseInt(parts[4]));
+    }
+    else if (instruction.startsWith('rotate left')) {
+        letters = rotateRight(letters, parseInt(parts[2]));
+    }
+    else if (instruction.startsWith('rotate right')) {
+        letters = rotateLeft(letters, parseInt(parts[2]));
+    }
+    else if (instruction.startsWith('move')) {
+        letters = move(letters, parseInt(parts[5]), parseInt(parts[2]));
+    }
+    else if (instruction.startsWith('rotate based')) {
+        letters = rotateBasedLeft(letters, parts[6]);
     }
 }
 
