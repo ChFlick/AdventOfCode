61d34b1b2816129b6a942499b272761ffe3c96ba
Christoph Flick <flick@bitside.de>
2019-07-29 08:38:26 +0200

    Add visualization of 2016-22.1

--- 2016/22/gridComputing.ts
+++ 2016/22/gridComputing.ts
@@ -23,19 +23,31 @@ const nodes = dfLines.map(line => line.split(/ +/)).map(split => new Node(
     parseInt(split[2].replace('T', '')),
     parseInt(split[3].replace('T', ''))));
 
-// const grid: Map<string, Node> = new Map();
-// nodes.forEach(node => grid.set(node.position.toString(), node));
-
-let viablePairs = 0;
+let viablePairs = [];
 for (const firstNode of nodes) {
     for (const secondNode of nodes) {
         if (firstNode === secondNode || firstNode.used === 0) {
             continue;
         }
         if (firstNode.used <= secondNode.avail) {
-            viablePairs++;
+            viablePairs.push(firstNode.position.toString());
         }
     }
 }
 
-console.log(viablePairs);
+console.log(viablePairs.length);
+
+// V2
+
+const grid: Map<string, Node> = new Map();
+nodes.forEach(node => grid.set(node.position.toString(), node));
+
+
+for (let x = 0; x < 35; x++) {
+    let t = "";
+    for (let y = 0; y < 29; y++) {
+        const node = nodes.find(n => n.position[0] === x && n.position[1] === y);
+        t += (node.avail > node.used ? '_' : (viablePairs.includes(node.position.toString()) ? "." : "#"))  + " ";
+    }
+    console.log(t);
+}
