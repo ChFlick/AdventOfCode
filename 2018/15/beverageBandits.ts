81ccc3402b6aa1886f99878386ba13051aecbfdd
Christoph Flick <christophflick@gmx.de>
2019-08-11 00:10:36 +0200

    Add shortest path function for 2018-15.1

--- 2018/15/beverageBandits.ts
+++ 2018/15/beverageBandits.ts
@@ -1,13 +1,13 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-type MapStuff = Elf | Goblin | Blocker | Free
+type Position = [number, number];
 class Entity {
-    position: [number, number]
+    position: Position
     hp: number
     dmg: number
 
-    constructor(position: [number, number]) {
+    constructor(position: Position) {
         this.position = position;
         this.hp = 200;
         this.dmg = 3;
@@ -26,6 +26,8 @@ class Goblin extends Entity { }
 class Elf extends Entity { }
 type Blocker = '#';
 type Free = '.';
+type MapObject = Elf | Goblin | Blocker | Free;
+
 
 const goblins: Goblin[] = [];
 const elves: Elf[] = [];
@@ -33,7 +35,7 @@ const elves: Elf[] = [];
 const fileBuffer: Buffer = fs.readFileSync(path.resolve(__dirname, 'input.txt'));
 const inputMap: string[] = fileBuffer.toString().split("\n");
 
-const map: MapStuff[][] = [];
+const map: MapObject[][] = [];
 inputMap.forEach(() => map.push([]));
 
 for (let y = 0; y < inputMap.length; y++) {
@@ -55,13 +57,39 @@ for (let y = 0; y < inputMap.length; y++) {
     }
 }
 
-const findClosestEntity = (position: [number, number], map: MapStuff[][], entityType: typeof Entity) => {
+const getPosVariations = (pos: Position): Position[] =>
+    [[pos[0] + 1, pos[1]],
+    [pos[0] - 1, pos[1]],
+    [pos[0], pos[1] + 1],
+    [pos[0], pos[1] - 1]];
 
+const stepsToClosestEntity = (position: Position, map: MapObject[][], searchGoblin: boolean): number => {
+    let currentPositions: Position[] = getPosVariations(position);
+    let steps = 0;
+    while (currentPositions.length > 0) {
+        const nextPositions: Position[] = [];
+        for (const pos of currentPositions) {
+            const ele = map[pos[0]][pos[1]];
+            if (ele instanceof (searchGoblin ? Goblin : Elf)) {
+                return steps;
+            } else if (ele === '#' || ele instanceof (searchGoblin ? Elf : Goblin)) {
+                continue;
+            }
+
+            nextPositions.push(...getPosVariations(pos));
+        }
+        
+        currentPositions = nextPositions;
+        steps++;
+    }
 }
 
 const order: Entity[] = map.reduce((arr, row) => arr.concat(row.filter(ele => ele instanceof Entity)), [])
-                           .filter((ele): ele is Entity => ele instanceof Entity); // last filter is only a typeguard
+    .filter((ele): ele is Entity => ele instanceof Entity); // last filter is only a typeguard
 
 order.forEach(entity => {
     // entity.moveToClosestEnemy(); => findClosestEntity(entity.position, map, Entity)
 });
+
+console.log(stepsToClosestEntity([5,7], map, false));
+
