7ec678dfd86c3ef19ed08bbbeebedd19fe09bd7e
Christoph Flick <christophflick@gmx.de>
2019-08-11 12:50:35 +0200

    Add not completely working solution (tests work but not the real input) for 2018-15.1

--- 2018/15/beverageBandits.ts
+++ 2018/15/beverageBandits.ts
@@ -24,21 +24,38 @@ class Entity {
     moveToClosestEnemy(map: MapObject[][]) {
         const positions = getPosVariations(this.position);
         const steps = positions.map(pos => stepsToClosestEntity(pos, map, this instanceof Elf));
-        if(steps.filter(s => s !== undefined).length === 0) {
+        if (steps.filter(s => s !== undefined).length === 0) {
             return;
         }
-        console.log(steps.indexOf(Math.min(...steps.filter(v => v !== undefined))));
-        
         const nextPosition = positions[steps.indexOf(Math.min(...steps.filter(v => v !== undefined)))];
-        console.log(positions, steps, nextPosition);
 
-        if(map[nextPosition[0]][nextPosition[1]] instanceof Entity) {
+        if (map[nextPosition[0]][nextPosition[1]] instanceof Entity) {
             return;
         }
 
         map[nextPosition[0]][nextPosition[1]] = this;
         map[this.position[0]][this.position[1]] = '.';
         this.position = nextPosition;
+    }
+
+    attackNextEnemy(map: MapObject[][]) {
+        const positions = getPosVariations(this.position);
+        const enemies: Entity[] = positions
+            .map(pos => map[pos[0]][pos[1]])
+            .filter((ele): ele is Entity => ele instanceof (this instanceof Elf ? Goblin : Elf));
+
+        if (enemies.length === 0) {
+            return;
+        }
+
+        const minHp = Math.min(...enemies.map(e => e.hp));
+        const enemyToAttack = enemies.filter(e => e.hp === minHp)[0];
+
+        this.attack(enemyToAttack);
+        if (enemyToAttack.isDead()) {
+            map[enemyToAttack.position[0]][enemyToAttack.position[1]] = '.';
+            printMap(map);
+        }
     }
 }
 
@@ -48,9 +65,6 @@ type Blocker = '#';
 type Free = '.';
 type MapObject = Elf | Goblin | Blocker | Free;
 
-
-const goblins: Goblin[] = [];
-const elves: Elf[] = [];
 
 const fileBuffer: Buffer = fs.readFileSync(path.resolve(__dirname, 'input.txt'));
 const inputMap: string[] = fileBuffer.toString().split("\n");
@@ -63,11 +77,9 @@ for (let y = 0; y < inputMap.length; y++) {
         const char = inputMap[x][y];
         if (char === 'G') {
             const goblin = new Goblin([x, y]);
-            goblins.push(goblin);
             map[x][y] = goblin;
         } else if (char === 'E') {
             const elf = new Elf([x, y]);
-            elves.push(elf);
             map[x][y] = elf;
         } else if (char === '#') {
             map[x][y] = '#';
@@ -93,6 +105,13 @@ const printMap = (map: MapObject[][]) => {
     }
 };
 
+const containsGoblinAndElf = (map: MapObject[][]) => {
+    const hasGoblin = map.find((row) => row.find(ele => ele instanceof Goblin));
+    const hasElf = map.find((row) => row.find(ele => ele instanceof Elf));
+
+    return hasGoblin && hasElf;
+};
+
 const getPosVariations = (pos: Position): Position[] =>
     [[pos[0] - 1, pos[1]],
     [pos[0], pos[1] - 1],
@@ -106,36 +125,45 @@ const stepsToClosestEntity = (position: Position, map: MapObject[][], searchGobl
     while (currentPositions.length > 0) {
         currentPositions.forEach(pos => visitedPositions.add(pos.toString()));
         const nextPositions: Position[] = [];
-        for (const pos of currentPositions) {
-            const ele = map[pos[0]][pos[1]];
+        for (const curr of currentPositions) {
+            const ele = map[curr[0]][curr[1]];
             if (ele instanceof (searchGoblin ? Goblin : Elf)) {
                 return steps;
             } else if (ele === '#' || ele instanceof (searchGoblin ? Elf : Goblin)) {
                 continue;
             }
 
-            nextPositions.push(...getPosVariations(pos));
+            // Avoid duplicates
+            getPosVariations(curr).forEach(pos => {
+                if (!nextPositions.find(nextPos => nextPos[0] === pos[0] && nextPos[1] === pos[1])) {
+                    nextPositions.push(pos);
+                }
+            });
         }
 
         currentPositions = nextPositions.filter(pos => !visitedPositions.has(pos.toString()));
+
         steps++;
     }
+}
+
+let rounds = 0;
+while (containsGoblinAndElf(map)) {
+    const order: Entity[] = map.reduce((arr, row) => arr.concat(row.filter(ele => ele instanceof Entity)), [])
+        .filter((ele): ele is Entity => ele instanceof Entity); // last filter is only a typeguard
+
+    for (const entity of order) {
+        if(entity.isDead()) { continue; }
+        entity.moveToClosestEnemy(map);
+        entity.attackNextEnemy(map);
+    };
+
+    // printMap(map);
+    rounds++;
 }
 
 const order: Entity[] = map.reduce((arr, row) => arr.concat(row.filter(ele => ele instanceof Entity)), [])
     .filter((ele): ele is Entity => ele instanceof Entity); // last filter is only a typeguard
 
-order.forEach(entity => {
-    entity.moveToClosestEnemy(map);
-});
-printMap(map);
-
-order.forEach(entity => {
-    entity.moveToClosestEnemy(map);
-});
-printMap(map);
-
-order.forEach(entity => {
-    entity.moveToClosestEnemy(map);
-});
-printMap(map);
+console.log(rounds - 1, order.reduce((hp, entity) => hp + entity.hp, 0));
+console.log((rounds - 1) * order.reduce((hp, entity) => hp + entity.hp, 0));
