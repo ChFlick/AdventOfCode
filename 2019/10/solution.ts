416cd54163c840709453d503aa04c8231237a089
Christoph Flick <christophflick@gmx.de>
2019-12-10 12:31:36 +0100

    Refactor solution for 10.1

--- 2019/10/solution.ts
+++ 2019/10/solution.ts
@@ -4,56 +4,30 @@ import * as path from 'path';
 const fileBuffer: Buffer = fs.readFileSync(path.resolve(__dirname, 'input.txt'));
 const asteroids: boolean[][] = fileBuffer.toString().split("\n").map(line => line.split('').map(v => v === '#'));
 
-const distance = (x1, y1, x2, y2) => Math.hypot(x2 - x1, y2 - y1);
-
-const isVisibleFrom = (ix: number, iy: number, tx: number, ty: number) => {
-    const distanceToTarget = distance(ix, iy, tx, ty);
-    for (let x = 0; x < asteroids.length; x++) {
-        for (let y = 0; y < asteroids[x].length; y++) {
-            if (!asteroids[x][y] || (ix === x && iy === y) || (tx === x && ty === y)) {
-                continue;
-            }
-
-            const distanceWithStep = distance(ix, iy, x, y) + distance(x, y, tx, ty);
-            if (Math.abs(distanceToTarget - distanceWithStep) < 0.000005) {
-                return false;
-            }
+const asteroidList: [number, number][] = [];
+for (let y = 0; y < asteroids.length; y++) {
+    for (let x = 0; x < asteroids[y].length; x++) {
+        if (asteroids[y][x]) {
+            asteroidList.push([x, y]);
         }
     }
-    return true;
 }
 
+const radiusBetween = (x1, y1, x2, y2) => Math.atan2(x2 - x1, y2 - y1) * 180 / Math.PI;
 const getVisibileAsteroidsFrom = (ix: number, iy: number) => {
-    let numberVisible = 0;
+    const radii = new Set();
     for (let x = 0; x < asteroids.length; x++) {
         for (let y = 0; y < asteroids[x].length; y++) {
             if (!asteroids[x][y]) {
                 continue;
             }
 
-            if (isVisibleFrom(ix, iy, x, y)) {
-                numberVisible++;
-            }
+            const angleDeg = radiusBetween(x, y, ix, iy);
+            radii.add(angleDeg);
         }
     }
-    return numberVisible;
+    return radii.size;
 }
 
-
-let maxN = 0;
-let maxVal;
-for (let x = 0; x < asteroids.length; x++) {
-    for (let y = 0; y < asteroids[x].length; y++) {
-        if (!asteroids[x][y]) {
-            continue;
-        }
-
-        const visibleAsteroids = getVisibileAsteroidsFrom(x, y) - 1;
-        if(visibleAsteroids > maxN) {
-            maxN = visibleAsteroids;
-            maxVal = [y, x];
-        }
-    }
-}
-
-console.log(maxN, maxVal);
+const visibleAsteroids = asteroidList.map(coords => getVisibileAsteroidsFrom(coords[0], coords[1]));
+console.log(Math.max(...visibleAsteroids));
