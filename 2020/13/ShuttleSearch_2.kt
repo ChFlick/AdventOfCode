e19f72bd7ff89bbaa610a392684e9e3fe3b1023d
Christoph Flick <ChFlick@users.noreply.github.com>
2020-12-16 11:25:24 +0100

    Add solution for 2020-13.2

--- 2020/13/ShuttleSearch_2.kt
+++ 2020/13/ShuttleSearch_2.kt
@@ -2,32 +2,41 @@ package `13`
 
 import java.io.File
 import java.util.concurrent.TimeUnit
-import kotlin.math.absoluteValue
 import kotlin.system.measureTimeMillis
 import kotlin.time.Duration
-import kotlin.time.measureTimedValue
 
-// BRUTE FORCE °,..,°
 @kotlin.time.ExperimentalTime
 fun main(args: Array<String>) {
+    infix fun Map.Entry<Long, Int>.isAlignedAt(t: Long): Boolean {
+        return (((t % this.key) + this.value) % this.key) == 0L
+    }
+
     val executionTime = measureTimeMillis {
         val input = File("${System.getProperty("user.dir")}/2020/13/input.txt")
-                .readLines()
+            .readLines()
 
         val busses = input[1].split(",")
-                .mapIndexed { index, value -> value to index }
-                .filterNot { it.first == "x" }
-                .map { it.first.toLong() to it.second }
-                .toMap()
+            .mapIndexed { index, value -> value to index }
+            .filterNot { it.first == "x" }
+            .map { it.first.toLong() to it.second }
+            .toMap()
 
-        val maxBus = busses.keys.maxOrNull() ?: 0
-        val firstPotentialNumberForMax = (100000000000000L..Long.MAX_VALUE).find {
-            (((it % maxBus) + busses[maxBus]!!) % maxBus) == 0L
-        }!!
+        var t = 1L
 
-        println((firstPotentialNumberForMax..Long.MAX_VALUE step maxBus).find { t ->
-            busses.all { (((t % it.key) + it.value) % it.key) == 0L}
-        })
+        while (!busses.all { it isAlignedAt t }) {
+            val alignedBusses = busses.filter { it isAlignedAt t }
+            val numberBussesAligned = alignedBusses.count()
+            val steplength = alignedBusses.keys.reduceOrNull { acc, i -> acc * i } ?: 1L
+
+            val x = (t..Long.MAX_VALUE step steplength)
+                .asSequence()
+                .onEach { t = it }
+                .map { currentTime -> busses.entries.filter { it isAlignedAt currentTime }.count() }
+                .find { it > numberBussesAligned }
+            println(x)
+        }
+        println("The result is $t")
     }
-    println(Duration.convert(executionTime.toDouble(), TimeUnit.MILLISECONDS, TimeUnit.SECONDS))
+    println(executionTime)
+
 }
