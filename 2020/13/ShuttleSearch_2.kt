a9a95bd7bd7fc3e31e8aa5a056e1ff6f82dd997a
Christoph Flick <ChFlick@users.noreply.github.com>
2020-12-16 10:07:05 +0100

    Add a non-terminating brute-force solution for 2020-13.2

---
+++ 2020/13/ShuttleSearch_2.kt
@@ -0,0 +1,32 @@
+package `13`
+
+import java.io.File
+import java.util.concurrent.TimeUnit
+import kotlin.math.absoluteValue
+import kotlin.system.measureTimeMillis
+import kotlin.time.Duration
+import kotlin.time.measureTimedValue
+
+@kotlin.time.ExperimentalTime
+fun main(args: Array<String>) {
+    val executionTime = measureTimeMillis {
+        val input = File("${System.getProperty("user.dir")}/2020/13/input.txt")
+                .readLines()
+
+        val busses = input[1].split(",")
+                .mapIndexed { index, value -> value to index }
+                .filterNot { it.first == "x" }
+                .map { it.first.toLong() to it.second }
+                .toMap()
+
+        val maxBus = busses.keys.maxOrNull() ?: 0
+        val firstPotentialNumberForMax = (100000000000000L..Long.MAX_VALUE).find {
+            ((it % maxBus) + busses[maxBus]!!) == maxBus
+        }!!
+
+        println((firstPotentialNumberForMax..Long.MAX_VALUE step maxBus).find { t ->
+            busses.all { ((t % it.key) + it.value) == it.key }
+        })
+    }
+    println(Duration.convert(executionTime.toDouble(), TimeUnit.MILLISECONDS, TimeUnit.SECONDS))
+}
