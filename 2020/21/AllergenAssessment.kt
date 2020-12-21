2a844123bc159239eb4fee1ae6c6ef82fd4130a3
Christoph Flick <ChFlick@users.noreply.github.com>
2020-12-21 09:34:02 +0100

    Add solution for 2020-21

---
+++ 2020/21/AllergenAssessment.kt
@@ -0,0 +1,46 @@
+package `21`
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    data class Dish(val ingredients: List<String>, val allergens: List<String>)
+
+    val dishes = File("${System.getProperty("user.dir")}/2020/21/input.txt")
+        .readLines()
+        .map {
+            Dish(
+                it.substringBefore(" (").split(" "),
+                it.substringAfter("(contains ").substringBefore(")").split(", ")
+            )
+        }
+
+    val allergens = dishes.flatMap { it.allergens }.toSet()
+
+
+    val allergensByIngredient = mutableMapOf<String, String>()
+
+    while (allergensByIngredient.size < allergens.size) {
+        val foundAllergens = allergens.mapNotNull { allergen ->
+            val potentialIngredients =
+                dishes.filter { it.allergens.contains(allergen) }
+                    .map { dish ->
+                        dish.ingredients.filter {
+                            it !in allergensByIngredient.values
+                        }.toSet()
+                    }
+                    .reduce { res, curr -> res.intersect(curr) }
+
+            if (potentialIngredients.size == 1)
+                allergen to potentialIngredients.first()
+            else
+                null
+        }
+        allergensByIngredient += foundAllergens
+    }
+
+    val ingredientsWithoutAllergens =
+        dishes.flatMap { it.ingredients }
+            .filter { it !in allergensByIngredient.values }
+
+    println(ingredientsWithoutAllergens.count())
+}
