23d16230323ee7259625c822c92f8651df42ca83
Christoph Flick <ChFlick@users.noreply.github.com>
2020-12-27 19:09:27 +0100

    Add solution of 2020-23.2
    Only the starting number was wrong...

--- 2020/23/CrabCups_2.kt
+++ 2020/23/CrabCups_2.kt
@@ -1,6 +1,7 @@
 package `23`
 
 import java.io.File
+import kotlin.system.measureTimeMillis
 
 fun main(args: Array<String>) {
     val SIZE        = 1000000
@@ -17,43 +18,43 @@ fun main(args: Array<String>) {
     }
 
     val deck = IntArray(SIZE)
-    val input = File("${System.getProperty("user.dir")}/2020/23/input.txt") 
-        .readText()
-        .split("")
-        .filter { it.isNotBlank() }
-        .map { it.toInt() }
 
-    input.zipWithNext { a, b -> deck[a - 1] = b }
-    deck[input.last() - 1] = 10
-    (10..SIZE).zipWithNext { a, b -> deck[a - 1] = b  }
-    deck[SIZE - 1] = input.first()
+    val time = measureTimeMillis {
+        val input = File("${System.getProperty("user.dir")}/2020/23/input.txt")
+            .readText()
+            .split("")
+            .filter { it.isNotBlank() }
+            .map { it.toInt() }
 
-    var currentCup = input.last()
-    repeat(TEN_MILLION) {
-        currentCup = deck[currentCup - 1]
+        input.zipWithNext { a, b -> deck[a - 1] = b }
+        deck[input.last() - 1] = input.size + 1
+        ((input.size + 1)..SIZE).zipWithNext { a, b -> deck[a - 1] = b }
+        deck[SIZE - 1] = input.first()
 
-        val nextCups = listOf(
-            deck[currentCup - 1],
-            deck[deck[currentCup - 1] - 1],
-            deck[deck[deck[currentCup - 1] -1] - 1]
-        )
+        var currentCup = SIZE
+        repeat(TEN_MILLION) {
+            currentCup = deck[currentCup - 1]
 
-        val offset = (1..4).find {
-            Math.floorMod(currentCup - it - 1, SIZE) + 1 !in nextCups
-        } ?: throw Error("doesnt work")
-        val destination = Math.floorMod(currentCup - offset - 1, SIZE) + 1
+            val nextCups = listOf(
+                deck[currentCup - 1],
+                deck[deck[currentCup - 1] - 1],
+                deck[deck[deck[currentCup - 1] - 1] - 1]
+            )
 
-        deck[currentCup - 1] = deck[nextCups.last() - 1]
-        deck[nextCups.last() - 1] = deck[destination - 1]
-        deck[destination - 1] = nextCups.first()
+            val offset = (1..4).find {
+                Math.floorMod(currentCup - it - 1, SIZE) + 1 !in nextCups
+            } ?: throw Error("doesnt work")
+            val destination = Math.floorMod(currentCup - offset - 1, SIZE) + 1
+
+            deck[currentCup - 1] = deck[nextCups.last() - 1]
+            deck[nextCups.last() - 1] = deck[destination - 1]
+            deck[destination - 1] = nextCups.first()
+        }
     }
-    deck.printStartingAt(934001, 5)
-
-    println(deck[934001])
+    println("Calculation took $time ms")
 
     println(deck[0])
-    println(deck[deck[0]])
+    println(deck[deck[0] - 1])
 
-    println(deck[0].toLong() * deck[deck[0]].toLong())
-
+    println(deck[0].toLong() * deck[deck[0] - 1].toLong())
 }
