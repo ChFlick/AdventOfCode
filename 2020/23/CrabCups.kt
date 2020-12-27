a31d244b9443490d24864c4116025e766a293084
Christoph Flick <ChFlick@users.noreply.github.com>
2020-12-27 14:52:35 +0100

    Improve solution for 2020-23.1

--- 2020/23/CrabCups.kt
+++ 2020/23/CrabCups.kt
@@ -3,61 +3,52 @@ package `23`
 import java.io.File
 
 fun main(args: Array<String>) {
-    class CircularList<T>(val list: MutableList<T>) : MutableList<T> by list {
-        override fun get(index: Int): T = list[index.safeIndex()]
+    val SIZE = 9
 
-        override fun removeAt(index: Int): T = list.removeAt(index.safeIndex())
-
-        override fun subList(fromIndex: Int, toIndex: Int): MutableList<T> {
-            return when {
-                fromIndex < toIndex -> list.subList(fromIndex.subListSafeIndex(), toIndex.subListSafeIndex())
-                fromIndex == toIndex -> mutableListOf()
-                else -> (list.subList(fromIndex.subListSafeIndex(), list.size) +
-                        list.subList(0, toIndex.subListSafeIndex())).toMutableList()
-            }
+    fun IntArray.printStartingAt(x: Int) {
+        val inOrder = IntArray(this.size)
+        var current = x
+        repeat(this.size) {
+            inOrder[it] = current
+            current = this[current - 1]
         }
-
-        override fun toString(): String = list.toString()
-
-        private fun Int.safeIndex(): Int = this % size
-        private fun Int.subListSafeIndex(): Int = this % (size + 1)
+        println(inOrder.asList())
     }
 
-    fun <T> List<T>.toCircular(): MutableList<T> = CircularList(this.toMutableList())
-
-    var deck = File("${System.getProperty("user.dir")}/2020/23/input.txt")
+    val deck = IntArray(SIZE)
+    val input = File("${System.getProperty("user.dir")}/2020/23/input.txt") 
         .readText()
         .split("")
         .filter { it.isNotBlank() }
         .map { it.toInt() }
-        .toCircular()
 
-    val cupsInOrder = deck.sortedDescending()
+    input.zipWithNext { a, b -> deck[a - 1] = b }
+    deck[input.last() - 1] = input.first()
 
-    var previousCup = -1
-    var currentCup = -1
+    var currentCup = input.last()
     repeat(100) {
-        fun currentIndex(x: Int? = null) = if (deck.indexOf(x ?: currentCup) == -1) 0 else deck.indexOf(x?: currentCup) + 1
-        fun nextIndex(x: Int? = null) = ((currentIndex(x) % deck.size) + 1) % deck.size
+        currentCup = deck[currentCup - 1]
 
-        previousCup = currentCup
-        currentCup = deck[currentIndex()]
         val nextCups = listOf(
-            deck.removeAt(nextIndex(previousCup)),
-            deck.removeAt(nextIndex(previousCup)),
-            deck.removeAt(nextIndex(previousCup))
+            deck[currentCup - 1],
+            deck[deck[currentCup - 1] - 1],
+            deck[deck[deck[currentCup - 1] - 1] - 1]
         )
 
-        val potentialDestinations = cupsInOrder.filterNot { it in nextCups }
-        val destination =
-            potentialDestinations[(potentialDestinations.indexOf(currentCup) + 1) % potentialDestinations.size]
+        val offset = (1..4).find {
+            Math.floorMod(currentCup - it - 1, SIZE) + 1 !in nextCups
+        } ?: throw Error("doesnt work")
+        val destination = Math.floorMod(currentCup - offset - 1, SIZE) + 1
 
-        deck = (deck.subList(0, deck.indexOf(destination) + 1) +
-                nextCups +
-                deck.subList(deck.indexOf(destination) + 1, deck.size)).toCircular()
+        deck[currentCup - 1] = deck[nextCups.last() - 1]
+        deck[nextCups.last() - 1] = deck[destination - 1]
+        deck[destination - 1] = nextCups.first()
 
-//        println("$destination, $nextCups")
-//        println(deck)
+        deck.printStartingAt(3)
+        println(nextCups)
+        println(destination)
     }
-    println((deck.subList(deck.indexOf(1), deck.size) + deck.subList(0, deck.indexOf(1))).joinToString(""))
+
+    deck.printStartingAt(1)
+
 }
