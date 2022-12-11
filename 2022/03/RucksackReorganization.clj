a48a1753527b9e9ed40e81f508b4c5ac86428ebf
Christoph Flick <ChFlick@users.noreply.github.com>
2022-12-11 17:08:01 +0000

    Add solution for 2022-03.2 in clojure

--- 2022/03/RucksackReorganization.clj
+++ 2022/03/RucksackReorganization.clj
@@ -9,11 +9,21 @@
 (def input (slurp "2022/03/input.txt"))
 (def rucksacks (str/split-lines input))
 
-(def duplicate-chars (map
+(def duplicates-in-halves (map
   (fn [[one two]]
       (first (setFn/intersection (set one) (set two))))
   (map
     #(split-at (/ (count %) 2) %) rucksacks)))
-(def result (reduce + (map char-code duplicate-chars)))
+(def result1 (reduce + (map char-code duplicates-in-halves)))
 
-(println result)
+(println result1)
+
+(def duplicates-in-triplets
+  (map
+    #(first (apply setFn/intersection %))
+    (partition 3 (map set rucksacks))))
+
+(def result2
+  (reduce + (map char-code duplicates-in-triplets)))
+
+(println result2)
